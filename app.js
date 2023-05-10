6920f928c4c867f5559e8dba176db9e5b9ba0f68
Lizandro_melo <joselizandro@souunisuam.com.br>
2023-05-09 21:03:15 -0300

    load

--- app.js
+++ app.js
@@ -1,25 +1,4 @@
-const getTypeColor = (type) => {
-  const normal = "#F5F5F5";
-  return (
-    {
-      normal,
-      fire: "#FDDFDF",
-      grass: "#DEFDE0",
-      electric: "#FCF7DE",
-      ice: "#DEF3FD",
-      water: "#DEF3FD",
-      ground: "#F4E7DA",
-      rock: "#D5D5D4",
-      fairy: "#FCEAFF",
-      poison: "#98D7A5",
-      bug: "#F8D5A3",
-      ghost: "#CAC0F7",
-      dragon: "#97B3E6",
-      psychic: "#EAEDA1",
-      fighting: "#E6E0D4",
-    }[type] || normal
-  );
-};
+
 
 // async, await and fetch
 // "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
@@ -51,7 +30,6 @@ const app = async () => {
       id: pokemon.id,
     }
     lista.append(criarLista(pokemonObj.nome, pokemonObj.type, pokemonObj.id));
-    
   });
 
 
@@ -76,5 +54,5 @@ const criarLista = (nome, type, id) => {
 };
 
 
-app();  
+app();
 
---
+++ pokedex.js
@@ -0,0 +1,79 @@
+const getTypeColor = (type) => {
+    const normal = "#F5F5F5";
+    return (
+        {
+            normal,
+            fire: "#FDDFDF",
+            grass: "#DEFDE0",
+            electric: "#FCF7DE",
+            ice: "#DEF3FD",
+            water: "#DEF3FD",
+            ground: "#F4E7DA",
+            rock: "#D5D5D4",
+            fairy: "#FCEAFF",
+            poison: "#98D7A5",
+            bug: "#F8D5A3",
+            ghost: "#CAC0F7",
+            dragon: "#97B3E6",
+            psychic: "#EAEDA1",
+            fighting: "#E6E0D4",
+        }[type] || normal
+    );
+};
+
+const loadPokemons = async () => {
+    const dataPokemons = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=15&offset=0"
+    );
+    const { results: pokemons } = await dataPokemons.json();
+    createCardPokemon(pokemons);
+};
+
+const createCardPokemon = (pokemonlist) => {
+    const lista = document.querySelector('#lista')
+    const fragment = document.createDocumentFragment();
+    pokemonlist.forEach(async (pokemon, id) => {
+        const { name, url } = pokemon;
+        getType = await fetch(url);
+        getType = await getType.json();
+        const { name: typeName } = getType.types[0].type;
+        pokemon = {
+            name,
+            type: typeName,
+            color: getTypeColor(typeName),
+            id,
+        };
+        const li = createLi(pokemon.name, pokemon.type, pokemon.color, pokemon.id);
+        fragment.append(li);
+        lista.append(fragment);
+    });
+};
+
+const createLi = (nome, type, color, id) => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+    li.setAttribute("class", "card");
+    li.style.setProperty("--type-color", color);
+    img.setAttribute("class", "card-image");
+    img.setAttribute("src",  `assets/img/(${id}).png`);
+    img.setAttribute("alt", nome);
+    h2.setAttribute("class", "card-title");
+    h2.textContent = nome;
+    p.setAttribute("class", "card-subtitle");
+    p.textContent = type;
+    li.append(img, p, h2);
+    return li;
+
+};
+
+const loadNextPokemons = () => {
+    
+}
+
+const executePokedex = async () => {
+    loadPokemons();
+};
+
+executePokedex();
